25b354f355cd6007366eca32e0c79a76a303b32b
Joss.Steward <joss.steward@gmail.com>
2015-08-31 10:52:45 -0400

    Further work on lab 1

---
+++ Steward_Lab1/src/Cell.java
@@ -0,0 +1,13 @@
+/**
+ * A Cell that can hold a LifeForm.
+ */
+public class Cell
+{
+   /**
+    * @return the LifeForm in this Cell.
+    */
+   public LifeForm getLifeForm()
+   {
+      return null;
+   }
+}
