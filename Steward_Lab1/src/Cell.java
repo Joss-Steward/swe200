99f22ec4e658f58bdae5c507145041c0040a287a
Joss.Steward <joss.steward@gmail.com>
2015-08-31 11:54:58 -0400

    Added Cell and TestCell

--- Steward_Lab1/src/Cell.java
+++ Steward_Lab1/src/Cell.java
@@ -3,11 +3,29 @@
  */
 public class Cell
 {
+	LifeForm lifeForm;
+	
    /**
     * @return the LifeForm in this Cell.
     */
    public LifeForm getLifeForm()
    {
-      return null;
+      return lifeForm;
    }
+   
+   /**
+    * Tries to add the LifeForm to the Cell.  
+    * Will not add the LifeForm is the Cell isn't empty.
+    * @return true if the LifeForm was added to the Cell, false otherwise. 
+    */
+   public boolean addLifeForm(LifeForm entity)
+   {
+	   if(lifeForm == null)
+	   {
+		   lifeForm = entity;
+		   return true;
+	   } else {
+		   return false;
+	   }
+   }   
 }
--- Steward_Lab1/src/TestCell.java
+++ Steward_Lab1/src/TestCell.java
@@ -16,4 +16,27 @@ public class TestCell
       Cell cell = new Cell();
       assertNull(cell.getLifeForm());
    }
+   
+   /**
+    * Checks to see if we change the LifeForm held by the Cell that
+    * getLifeForm properly responds to this change.
+    */
+   @Test
+   public void testSetLifeForm()
+   {
+	   LifeForm bob = new LifeForm("Bob", 40);
+	   LifeForm fred = new LifeForm("Fred", 40);
+	   Cell cell = new Cell();
+	   
+	   //The cell is empty right now, so this will work
+	   boolean success = cell.addLifeForm(bob);
+	   assertTrue(success);
+	   assertEquals(bob, cell.getLifeForm());
+	   
+	   //The cell is not empty now, so this should fail
+	   success = cell.addLifeForm(fred);
+	   assertFalse(success);
+	   assertEquals(bob, cell.getLifeForm());
+   }
+   
 }
