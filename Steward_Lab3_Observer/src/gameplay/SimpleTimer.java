4ce74869c544997d1303060196400c646659fb9f
Joss.Steward <joss.steward@gmail.com>
2015-09-16 12:38:46 -0400

    On page 8 right now

--- Steward_Lab3_Observer/src/gameplay/SimpleTimer.java
+++ Steward_Lab3_Observer/src/gameplay/SimpleTimer.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
  * A simple implementation of the Observable Timer
  * @author Joss Steward
  */
-public class SimpleTimer implements Timer {
+public class SimpleTimer extends Thread implements Timer {
 	private ArrayList<TimeObserver> Observers;
 	private int time;
 	
@@ -13,6 +13,24 @@ public class SimpleTimer implements Timer {
 		Observers = new ArrayList<TimeObserver>();
 		time = 0;
 	}
+	
+	/**
+	 * This timer will tick every once in a while,
+	 * calling timeChanged() to notify its observers
+	 * that time has moved on
+	 */
+	public void run(){
+		for(int i = 0; i < 100; i++){
+			try
+			{
+				Thread.sleep(1000);
+				this.timeChanged();
+			} catch (InterruptedException e) {
+				System.out.println("Oh god! Oh god! Everything is on fire! Help!");
+			}
+		}
+	}
+	
 	
 	/** 
 	 * @return The current in-game time
